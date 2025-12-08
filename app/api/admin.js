f2b05b56e791ce2d59a5817472916421528524c2
LGD_Sunday <12055071971@qq.com>
2025-12-09 06:17:40 +0800

    feat：修改接口名与数据返回策略

--- app/api/admin.js
+++ app/api/admin.js
@@ -2,7 +2,7 @@
  * 返回当前时间点进行模拟面试的人数
  */
 export const getMockInterviewCountAPI = ($api) => {
-	return $api('/admin/mock-interview-count', {
+	return $api('/admin/interview-count', {
 		method: 'GET'
 	})
 }
